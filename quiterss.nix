ec1bbe01ed5bb5cec518f5fcda7a2af091a5abcb
sjau <github.com@sjau.ch>
2017-06-25 14:44:45 +0200

    quite rss update to 0.18.6

--- quiterss.nix
+++ quiterss.nix
@@ -2,16 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "QuiteRSS-${version}";
-  version = "0.18.4";
+  version = "0.18.6";
 
-#  sourceRoot = ".";
-  sourceRoot = "quiterss-0.18.4";
-  
+  sourceRoot = ".";
+
   src = fetchurl {
-#    url = "http://quiterss.org/files/0.18.4/QuiteRSS-0.18.4-src.tar.gz";
-    url = "https://github.com/QuiteRSS/quiterss/archive/0.18.4.tar.gz";
-#    sha256 = "e53ddcab32ed4894ee59afd0db5d7ab86248986fdf6e1c1aeec9c8a841867a9c";
-    sha256 = "df1de257561160f18ef5e86bd9eb6d28b2044814db16df061b9c881607481629";
+    url = "https://quiterss.org/files/0.18.6/QuiteRSS-0.18.6-src.tar.gz";
+    sha256 = "1162a7878110f8fab0257d31bcfd37ebcf9610ddefaf3f35cd4af8b606a1f066";
   };
 
   buildInputs = [ qt5.qtbase sqlite pkgconfig qt5.qtwebkit qt5.qtmultimedia qt5.qttools qt5.qmakeHook ];
